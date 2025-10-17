e35d4abfd4eb214ec68cd9dd6e80937639e69f95
Bibek <mr.bibekdahal@gmail.com>
2025-10-17 22:44:39 +0545

    Add files via upload

---
+++ ex2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "-------------" << endl << "My name is Dell" << endl
+		<< "I am a laptop" << endl << "I am debugging " << endl << "-------------" << endl;
+}
