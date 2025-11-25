8306846c159a70af2f5df974bd60510a331ebb52
Bibek <mr.bibekdahal@gmail.com>
2025-11-25 12:05:09 +0545

    Add files via upload

---
+++ value of x.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x1 = 15 / 4,
+	x2 = 89 % 12,
+	x3  = 35;
+	x3 += 7;
+	x3 -= 7;
+	x3 *= 7;
+	x3 /= 7;
+
+	cout << "Value of Q1 is: "<< x1<<  endl;
+	cout << "Value of Q2 is: " << x2 << endl;
+	cout << "Value of Q3 is: " << x3 << endl;
+
+	
+
+	return 0;
+}
