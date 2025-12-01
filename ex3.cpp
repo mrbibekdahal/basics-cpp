484e1259948231d0dd49a28b89a0b184f882c274
Bibek <mr.bibekdahal@gmail.com>
2025-12-01 14:11:27 +0545

    Add files via upload

---
+++ ex3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	double wages1;
+	double wages2;
+	double total;
+	
+	wages1 = 10.50;
+    wages2 = 35.63;
+	total = wages1 + wages2;
+	cout << "Total Wages = " <<total << endl;
+
+
+}
