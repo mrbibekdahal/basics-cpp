05e7d729ea4f001a3a0aa4d80cb4454c4927fdea
Bibek <mr.bibekdahal@gmail.com>
2025-12-06 18:05:12 +0545

    Add files via upload

---
+++ Ex5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int Days_in_Week = 7;
+	int age = 20;
+	double height = 1.75;
+	char grade = 'A';
+
+	cout << "Age: " << age << endl;
+	cout << "Height: " << height << endl;
+	cout << "Grade: " << grade << endl;
+	cout << "Days in a week: " << Days_in_Week << endl;
+
+	return 0;
+
+}
